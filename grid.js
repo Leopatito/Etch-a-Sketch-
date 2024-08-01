ed9505dd442316ab583c73b81229652ce7e5abe7
leo <leotaikitamura@gmail.com>
2024-08-01 17:33:04 -0300

    project finished

--- grid.js
+++ grid.js
@@ -1,9 +1,11 @@
+const numbers = parseInt(prompt("Enter a value:"));
+
 
 const big = document.querySelector(".big-square");
-for (index = 0; index < 16; index++) {
+for (index = 0; index < numbers; index++) {
     var newDiv = document.createElement("div");
     
-    for (let indexj = 0; indexj < 16; indexj++) {
+    for (let indexj = 0; indexj < numbers; indexj++) {
         var newDivInAnotherDiv = document.createElement("div");
         newDiv.appendChild(newDivInAnotherDiv);
         
@@ -15,4 +17,29 @@ miniBoxes.forEach(box => {
     box.addEventListener("mouseover", () => {
         box.style.backgroundColor = "blue";
     });
-});
+}); 
+
+const btn = document.querySelector("#novo");
+
+btn.addEventListener("click", () => {
+    const removeDiv = document.getElementById("novo");
+    remove.innerHTML = "";
+    const numbers = parseInt(prompt("Enter a value:"));
+    const big = document.querySelector(".big-square");
+    for (index = 0; index < numbers; index++) {
+        var newDiv = document.createElement("div");
+        
+        for (let indexj = 0; indexj < numbers; indexj++) {
+            var newDivInAnotherDiv = document.createElement("div");
+            newDiv.appendChild(newDivInAnotherDiv);
+            
+        }
+        big.appendChild(newDiv);
+    }
+    var miniBoxes = document.querySelectorAll('.big-square div div');    
+    miniBoxes.forEach(box => {
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "blue";
+        });
+    }); 
+  });
